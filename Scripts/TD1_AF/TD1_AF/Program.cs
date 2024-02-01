ec3dab30848919753525d3ff46014513e14a590b
AlexandreFrancony <alexandre.francony05@gmail.com>
2024-02-01 08:43:23 +0100

    Init + TD1

---
+++ Scripts/TD1_AF/TD1_AF/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+public class Document
+{
+    public required string  NumeroEnregistrement { get; set; }
+    public required string Titre { get; set; }
+    public required int NombrePages { get; set; }
+    public required string Editeur { get; set; }
+}
