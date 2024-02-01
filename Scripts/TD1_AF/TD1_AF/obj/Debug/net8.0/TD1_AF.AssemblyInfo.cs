4156f81d2e33e6da4e4123f7bee38ff099b822bf
AlexandreFrancony <alexandre.francony05@gmail.com>
2024-02-01 09:47:23 +0100

    TD1 done

--- Scripts/TD1_AF/TD1_AF/obj/Debug/net8.0/TD1_AF.AssemblyInfo.cs
+++ Scripts/TD1_AF/TD1_AF/obj/Debug/net8.0/TD1_AF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TD1_AF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+117f0dad58a946e869fc6af60c8ccd482c9f7930")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec3dab30848919753525d3ff46014513e14a590b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TD1_AF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TD1_AF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
